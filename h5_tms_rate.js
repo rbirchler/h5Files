f833c959b4f4d2fad1b3af1b78f42e6c8120fa22
rbirchler <regan@habit5.com>
2025-08-04 17:42:10 -0500

    Update h5_tms_rate.js

--- h5_tms_rate.js
+++ h5_tms_rate.js
@@ -1,1 +1,19 @@
-"function(a, b) { return a + b; }"
+{
+  "getFedExBearerToken": "function(https, log, clientId, clientSecret) { \
+    var headers = {}; \
+    headers['Content-Type'] = 'application/x-www-form-urlencoded'; \
+    \
+    var body = 'grant_type=client_credentials&client_id=' + encodeURIComponent(clientId) + \
+               '&client_secret=' + encodeURIComponent(clientSecret); \
+    \
+    var response = https.post({ \
+      url: 'https://apis.fedex.com/oauth/token', \
+      body: body, \
+      headers: headers \
+    }); \
+    \
+    var token = JSON.parse(response.body).access_token; \
+    log.debug({ title: 'FedEx Bearer Token', details: token }); \
+    return token; \
+  }"
+}
