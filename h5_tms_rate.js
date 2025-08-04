b2dd6b0195ae141b3a118645e6506c35d162e242
rbirchler <regan@habit5.com>
2025-08-04 16:48:45 -0500

    Add files via upload

---
+++ h5_tms_rate.js
@@ -0,0 +1,31 @@
+/**
+ * This module exposes a function that will assist in queuing a scheduled script
+ * or a map/reduce script.
+ *
+ * @NApiVersion 2.1
+ */
+define(['N/record', 'N/search', 'N/task'],
+    /**
+     * @param{record} record
+     * @param{search} search
+     */
+    (record, search, task) => {
+        const obj = {}
+        /**
+         * This function launches a map/reduce OR a scheduled script.
+         *
+         * @param paramScriptId
+         * @param paramParameters
+         * @param isScheduledScript
+         * @returns {*}
+         */
+        const launchRate = () => {
+            log.debug({
+                title:"HERE",
+                details: "HERE"
+            })
+        }
+
+        return {launchRate}
+
+    });
