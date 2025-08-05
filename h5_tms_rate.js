6c3ba207e9caa08de4206163d304afd901547296
rbirchler <regan@habit5.com>
2025-08-05 12:21:22 -0500

    Update h5_tms_rate.js

--- h5_tms_rate.js
+++ h5_tms_rate.js
@@ -1,3 +1,11 @@
 {
-  "add": "function(a, b) { return a + b; }"
+ns: {
+    name: "User-Agent",
+    value: "Mozilla/5.0"
+  },
+abf: {
+     "Content-Type": "text/xml",
+     "SOAPaction": "https://www.abfs.com/xml/aquotexml.asp",
+   }
 }
+
